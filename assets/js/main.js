59457ad817493c5a5d4002aa389deb1adf874d98
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-10-31 20:17:56 -0400

    detail data

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 const sectionPrincipal = document.getElementById('principal')
 const sectionDetail = document.getElementById('detail')
+const dataPokemonDetails = document.getElementById('dataPokemonDetails')
 
 const maxRecords = 151
 const limit = 10
@@ -25,11 +26,38 @@ function convertPokemonToLi(pokemon) {
     `
 }
 
+function convertPokemonDetail(pokemon) {
+    return `
+        <div class="pokemonDetailPage ${pokemon.type}" onClick="openDetail(event,${pokemon.number})">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </div>
+    `
+}
+
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map(convertPokemonToLi).join('')
         pokemonList.innerHTML += newHtml
     })
+}
+
+function loadDataPokemonDetail(id) {
+    pokeApi.getPokemonDetailPage(id)
+        .then((data) => {
+            console.log('data', data);
+            /* const newHtml = pokemons.map(convertPokemonToLi).join('')
+            dataPokemonDetails.innerHTML += newHtml */
+        })
 }
 
 loadPokemonItens(offset, limit)
@@ -51,14 +79,10 @@ loadMoreButton.addEventListener('click', () => {
 function openDetail(event, idPokemon) {
     sectionPrincipal.style.display = 'none';
     sectionDetail.style.display = 'block';
-    /* sectionDetail.style.removeProperty('display');
-    sectionPrincipal.style.removeProperty('display'); */
+    loadDataPokemonDetail(idPokemon);
 }
 
 function voltar(event) {
     sectionPrincipal.style.display = 'block';
     sectionDetail.style.display = 'none';
-    /* sectionPrincipal.style.removeProperty('display');
-    sectionDetail.style.removeProperty('display');
-    sectionDetail.style.display = 'none'; */
 }
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -33,3 +33,12 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
 }
+
+pokeApi.getPokemonDetailPage = (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => {console.log('aqui',jsonBody)})
+        //.then((pokemonDetail) => {console.log('pokemonDetail',pokemonDetail)})
+}
